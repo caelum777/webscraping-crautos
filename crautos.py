698baf59c05f3425881877bc874b2fa2359ad0e6
Lesmed Gutierrez <lesmedgutierrez@outlook.com>
2021-11-27 00:05:08 -0600

    refactor: modularization

--- crautos.py
+++ crautos.py
@@ -1,11 +1,12 @@
+import json
 import logging
-import pprint
 import pandas as pd
 import requests
 
 from bs4 import BeautifulSoup
 from bs4.element import Tag, NavigableString
 from datetime import date, datetime, timedelta
+
 
 formdata = {
     "brand": 97,  # 35 toyota, 16 hyundai, 15 honda, 97 BYD,
@@ -56,7 +57,7 @@ INFO_GENERAL = [
 
 COLUMNS_ORDER = [
     'Modelo', 'Estilo', 'Combustible', 'Transmisión', 
-    'Cilindrada', 'Kilometraje', 'Año', 'Precio Colones', 'Precio Dolares', 'Precio negociable',
+    'Año','Cilindrada', 'Kilometraje',  'Precio Colones', 'Precio Dolares', 'Precio negociable',
     'Placa', 'Color exterior', 'Color interior', 'Ya pagó impuestos', 'Provincia', 'Estado',
     'Fecha de ingreso', 'link', 'Se recibe vehículo', 'de puertas'
     ]
@@ -81,24 +82,24 @@ def read_recursive(tags, data):
             tag_py_str = "".join(e for e in str(tag) if e.isalnum() or e == " ")
             data.append(tag_py_str)
         
-def parse_fecha(fecha_str):
-    fecha = fecha_str.replace('de', '').replace('l', '')
-    fecha = fecha.split()
-    fecha[1] = months[fecha[1]]
-    fecha = ' '.join(fecha)
-    fecha = datetime.strptime(fecha, '%d %m %Y').date()
+def parse_date(date_str):
+    date_ = date_str.replace('de', '').replace('l', '')
+    date_ = date_.split()
+    date_[1] = months[date_[1]]
+    date_ = ' '.join(date_)
+    date_ = datetime.strptime(date_, '%d %m %Y').date()
 
-    return fecha
+    return date_
 
 
-def extract_str_to_int(series):
+def extract_str_to_int(series: pd.Series) -> pd.Series:
     series = pd.to_numeric((series.str.extract('(\d+)', expand=False)), errors="coerce", downcast="integer")
     series = series.fillna(0).astype(int)
 
     return series
 
 
-def get_posted_cars_links():
+def get_posted_cars_links(formdata):
         
     # Get posted cars links
     car_links = []
@@ -124,44 +125,66 @@ def get_posted_cars_links():
         current_page += 1
 
     logging.info("Number of pages: [{}]".format(max_page))
-    leather_car_links = []
-    df_list = []
-    # iterar cada car link
-    for car_link in car_links:
 
-        page = requests.get(CARDETAIL_URL.format(car_link))
-        soup = BeautifulSoup(page.content, 'html.parser')
+    return car_links
 
-        data = {"cardetails": [], "info": [], "equipamiento": [], "detalles": []}
 
-        read_recursive(soup.find_all('div', id='geninfo'), data["info"])
-        read_recursive(soup.find_all('div', {"class":"margin-bottom-10 clearfix cardetailtitle"}), data["detalles"])
+def pull_info_from_car_link(car_link) -> pd.DataFrame:
 
-        df = pd.DataFrame({"caracteristicas":data["info"]})
+    page = requests.get(CARDETAIL_URL.format(car_link))
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-        df["caracteristicas"] = df["caracteristicas"].str.strip()
-        df["valor"] = df["caracteristicas"].shift(-1)
-        
-        df = df[df["caracteristicas"].isin(INFO_GENERAL)]
-        df = df.set_index("caracteristicas")
+    data = {"cardetails": [], "info": [], "equipamiento": [], "detalles": []}
 
-        df = df.transpose()
+    read_recursive(soup.find_all('div', id='geninfo'), data["info"])
+    read_recursive(soup.find_all('div', {"class":"margin-bottom-10 clearfix cardetailtitle"}), data["cardetails"])
 
-        df["Modelo"] = data["detalles"][0]
-        df["Año"] = data["detalles"][1]
-        df["Precio Colones"] = data["detalles"][2]
-        df["Precio Dolares"] = data["detalles"][3]
-        
-        df["link"] = CARDETAIL_URL.format(car_link)
+    df = pd.DataFrame({"caracteristicas":data["info"]})
 
-        df_list.append(df)
-
-        leather_car_links.append(CARDETAIL_URL.format(car_link))
+    df["caracteristicas"] = df["caracteristicas"].str.strip()
+    df["valor"] = df["caracteristicas"].shift(-1)
     
-    # concat todos los dfs
-    df = pd.concat(df_list, ignore_index=True)
+    df = df[df["caracteristicas"].isin(INFO_GENERAL)]
+    df = df.set_index("caracteristicas")
 
-    df["Fecha de ingreso"] = df["Fecha de ingreso"].apply(parse_fecha)
+    df = df.transpose()
+
+    df["Modelo"] = data["cardetails"][0]
+    df["Año"] = data["cardetails"][1]
+    df["Precio Colones"] = data["cardetails"][2]
+    df["Precio Dolares"] = data["cardetails"][3]
+    
+    df["link"] = CARDETAIL_URL.format(car_link)
+
+    return df
+
+
+
+
+def iter_car_links(car_links) -> pd.DataFrame:
+
+    df_list = []
+
+    # iter every car link
+    for car_link in car_links:
+        try:
+            df_car = pull_info_from_car_link(car_link)
+            df_list.append(df_car)
+        except Exception as exc:
+            print(exc)
+    
+    # concat every dfs
+
+    try:
+        df = pd.concat(df_list)
+    except Exception as exc:
+        print(exc)
+        for df1 in df_list:
+            print(len(df1.columns), df1.columns)
+        return pd.DataFrame()
+
+
+    df["Fecha de ingreso"] = df["Fecha de ingreso"].apply(parse_date)
 
     df["Cilindrada"] = extract_str_to_int(df["Cilindrada"])
     df["Kilometraje"] = extract_str_to_int(df["Kilometraje"])
@@ -170,16 +193,90 @@ def get_posted_cars_links():
     df["Año"] = extract_str_to_int(df["Año"])
 
 
-    file_datetime_id = datetime.today().strftime('%Y%m%d_%H%M%S')
-    
-    # df.to_csv(CSV_LOCATION+file_datetime_id+".csv", index=False)
+    return df
 
+
+def save_to_file(df:pd.DataFrame):
+    
     df = df[COLUMNS_ORDER]
+
+    file_datetime_id = datetime.today().strftime('%Y%m%d_%H%M%S')
     df.to_excel(CSV_LOCATION+file_datetime_id+".xlsx", index=False)
 
 
+def read_car_brands():
+    car_brands: dict
+    try: 
+        with open("car_brands.json") as json_file:
+            car_brands = json.load(json_file)
+    except Exception as exc:
+        print(exc)
+    finally:
+        return car_brands
+
+
 def main():
-    get_posted_cars_links()
+    # 35 toyota, 16 hyundai, 15 honda, 26 nissan, 17 isuzu, 34 suzuki, 19 kia
+    custom_brand_query = [
+        {"brand": 35, "modelstr": "rav4"},
+        {"brand": 35, "modelstr": "yaris"},
+        {"brand": 35, "modelstr": "hilux"},
+        {"brand": 35, "modelstr": "corolla"},
+        
+        {"brand": 16, "modelstr": "tucson"},
+        {"brand": 16, "modelstr": "santa fe"},
+        {"brand": 16, "modelstr": "santa fe"},
+        {"brand": 16, "modelstr": "creta fe"},
+
+        {"brand": 15, "modelstr": "crv"},
+
+        {"brand": 26, "modelstr": "kiks"},
+        {"brand": 26, "modelstr": "qashqai"},
+        {"brand": 26, "modelstr": "xtrail"},
+
+
+        {"brand": 17, "modelstr": "dmax"},
+
+        {"brand": 34, "modelstr": "vitara"},
+
+        {"brand": 19, "modelstr": "sportage"},
+        {"brand": 19, "modelstr": "sorento"},
+        
+        
+    ]
+
+    df_cars = []
+    car_brands = read_car_brands()
+    for e, custom_query in enumerate(custom_brand_query):
+
+
+        brand = car_brands.get(str(custom_query["brand"]), "Sin identificar")
+        car_model = custom_query.get("modelstr", "Sin identificar")
+
+        print(f"Iteration {e} of {len(custom_brand_query)}. Brand {brand} model: {car_model}")
+
+        formdata_local = formdata
+        formdata_local.update(custom_query)
+
+        car_links = get_posted_cars_links(formdata_local)
+
+        df = iter_car_links(car_links)
+
+        df["Marca"] = brand
+
+        df_cars.append(df)
+
+    if df_cars :
+    
+        df_cars = pd.concat(df_cars)
+
+        df_cars = df_cars.reset_index(drop=True)
+
+        save_to_file(df_cars)
+    else:
+
+        print("WTF no cars")
+
 
 if __name__ == "__main__":
     main()
